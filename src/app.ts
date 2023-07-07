5e5a15d1cc32a66b4ab0c2f57826054b696583ad
Paul Valdez <18030566@itcelaya.edu.mx>
2023-07-07 17:56:14 -0600

    Implementación de todos los componentes PrimeVue en el archivo src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,209 @@
 import type { App } from "vue";
-// import Button from "primevue/button";
-// import Menu from "primevue/menu";
 import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 import { RouterLink } from "vue-router";
 
+import Accordion from "primevue/accordion";
+import AccordionTab from "primevue/accordiontab";
+import AutoComplete from "primevue/autocomplete";
+import Avatar from "primevue/avatar";
+import AvatarGroup from "primevue/avatargroup";
+import Badge from "primevue/badge";
+import BadgeDirective from "primevue/badgedirective";
+import BlockUI from "primevue/blockui";
+import Breadcrumb from "primevue/breadcrumb";
+import Button from "primevue/button";
+import Calendar from "primevue/calendar";
+import Card from "primevue/card";
+import Carousel from "primevue/carousel";
+import CascadeSelect from "primevue/cascadeselect";
+import Chart from "primevue/chart";
+import Checkbox from "primevue/checkbox";
+import Chip from "primevue/chip";
+import Chips from "primevue/chips";
+import ColorPicker from "primevue/colorpicker";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup";
+import ConfirmDialog from "primevue/confirmdialog";
+import ConfirmPopup from "primevue/confirmpopup";
+import ContextMenu from "primevue/contextmenu";
+import DataTable from "primevue/datatable";
+import DataView from "primevue/dataview";
+import DataViewLayoutOptions from "primevue/dataviewlayoutoptions";
+import DeferredContent from "primevue/deferredcontent";
+import Dialog from "primevue/dialog";
+import Divider from "primevue/divider";
+import Dock from "primevue/dock";
+import Dropdown from "primevue/dropdown";
+import DynamicDialog from "primevue/dynamicdialog";
+import Editor from "primevue/editor";
+import Fieldset from "primevue/fieldset";
+import FileUpload from "primevue/fileupload";
+import FocusTrap from "primevue/focustrap";
+import Galleria from "primevue/galleria";
+import Image from "primevue/image";
+import InlineMessage from "primevue/inlinemessage";
+import Inplace from "primevue/inplace";
+import InputMask from "primevue/inputmask";
+import InputNumber from "primevue/inputnumber";
+import InputSwitch from "primevue/inputswitch";
+import InputText from "primevue/inputtext";
+import Knob from "primevue/knob";
+import Listbox from "primevue/listbox";
+import MegaMenu from "primevue/megamenu";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Message from "primevue/message";
+import MultiSelect from "primevue/multiselect";
+import OrderList from "primevue/orderlist";
+import OrganizationChart from "primevue/organizationchart";
+import OverlayPanel from "primevue/overlaypanel";
+import Paginator from "primevue/paginator";
+import Panel from "primevue/panel";
+import PanelMenu from "primevue/panelmenu";
+import Password from "primevue/password";
+import PickList from "primevue/picklist";
+import ProgressBar from "primevue/progressbar";
+import ProgressSpinner from "primevue/progressspinner";
+import RadioButton from "primevue/radiobutton";
+import Rating from "primevue/rating";
+import Ripple from "primevue/ripple";
+import Row from "primevue/row";
+import ScrollPanel from "primevue/scrollpanel";
+import ScrollTop from "primevue/scrolltop";
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Skeleton from "primevue/skeleton";
+import Slider from "primevue/slider";
+import SpeedDial from "primevue/speeddial";
+import SplitButton from "primevue/splitbutton";
+import Splitter from "primevue/splitter";
+import SplitterPanel from "primevue/splitterpanel";
+import Steps from "primevue/steps";
+import StyleClass from "primevue/styleclass";
+import TabMenu from "primevue/tabmenu";
+import TabPanel from "primevue/tabpanel";
+import TabView from "primevue/tabview";
+import Tag from "primevue/tag";
+import Terminal from "primevue/terminal";
+import TerminalService from "primevue/terminalservice";
+import Textarea from "primevue/textarea";
+import TieredMenu from "primevue/tieredmenu";
+import Timeline from "primevue/timeline";
+import Toast from "primevue/toast";
+import ToggleButton from "primevue/togglebutton";
+import Toolbar from "primevue/toolbar";
+import Tooltip from "primevue/tooltip";
+import Tree from "primevue/tree";
+import TreeSelect from "primevue/treeselect";
+import TreeTable from "primevue/treetable";
+import TriStateCheckbox from "primevue/tristatecheckbox";
+import VirtualScroller from "primevue/virtualscroller";
+
 const components = {
-  // Button,
-  // Menu,
   PrimeVue,
   RouterLink,
+  Accordion,
+  AccordionTab,
+  AutoComplete,
+  Avatar,
+  AvatarGroup,
+  Badge,
+  BlockUI,
+  Breadcrumb,
+  Button,
+  Calendar,
+  Card,
+  Carousel,
+  CascadeSelect,
+  Chart,
+  Checkbox,
+  Chip,
+  Chips,
+  ColorPicker,
+  Column,
+  ColumnGroup,
+  ConfirmDialog,
+  ConfirmPopup,
+  ContextMenu,
+  DataTable,
+  DataView,
+  DataViewLayoutOptions,
+  DeferredContent,
+  Dialog,
+  Divider,
+  Dock,
+  Dropdown,
+  DynamicDialog,
+  Editor,
+  Fieldset,
+  FileUpload,
+  Galleria,
+  Image,
+  InlineMessage,
+  Inplace,
+  InputMask,
+  InputNumber,
+  InputSwitch,
+  InputText,
+  Knob,
+  Listbox,
+  MegaMenu,
+  Menu,
+  Menubar,
+  Message,
+  MultiSelect,
+  OrderList,
+  OrganizationChart,
+  OverlayPanel,
+  Paginator,
+  Panel,
+  PanelMenu,
+  Password,
+  PickList,
+  ProgressBar,
+  ProgressSpinner,
+  RadioButton,
+  Rating,
+  Row,
+  ScrollPanel,
+  ScrollTop,
+  SelectButton,
+  Sidebar,
+  Skeleton,
+  Slider,
+  SpeedDial,
+  SplitButton,
+  Splitter,
+  SplitterPanel,
+  Steps,
+  TabMenu,
+  TabPanel,
+  TabView,
+  Tag,
+  Terminal,
+  TerminalService,
+  Textarea,
+  TieredMenu,
+  Timeline,
+  Toast,
+  ToggleButton,
+  Toolbar,
+  Tree,
+  TreeSelect,
+  TreeTable,
+  TriStateCheckbox,
+  VirtualScroller,
 };
+
 export default (app: App) => {
-  app.use(PrimeVue);
+  app.use(PrimeVue, { inputStyle: "filled", ripple: true });
+  app.use(ToastService);
+  app.directive("badge", BadgeDirective);
+  app.directive("focustrap", FocusTrap);
+  app.directive("tooltip", Tooltip);
+  app.directive("ripple", Ripple);
+  app.directive("styleclass", StyleClass);
   for (const [key, value] of Object.entries(components)) {
     app.component(key, value);
   }
--- src/pages/indexVue.vue
+++ src/pages/indexVue.vue
@@ -1,18 +1,52 @@
 <template>
   <div>
-    <h1>HERE is VueJS.</h1>
-    <p>El valor del contador es: {{ cont }}</p>
-    <PrimeButton label="+" @click="cont++" />
-    <PrimeButton label="-" @click="cont--" />
+    <h2>Lorem temporal de ejemplo:</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas error dicta, necessitatibus quos magnam nobis reprehenderit minima possimus,
+      sint quia esse quasi dolor totam ullam adipisci, quae veritatis in eveniet!
+    </p>
+    <div class="card flex justify-content-center">
+      <Toast />
+      <Button label="Show" @click="show()" />
+    </div>
+    <br />
+    <div class="card card-container flex justify-content-center align-items-center">
+      <div
+        v-ripple
+        class="p-ripple bg-primary flex select-none justify-content-center align-items-center shadow-2 border-round p-6 font-bold"
+      >Default</div>
+    </div>
+    <br />
+    <div class="card flex flex-column align-items-center gap-3">
+      <Button
+        v-styleclass="{ selector: '@next', toggleClass: 'p-disabled' }"
+        label="Toggle p-disabled"
+      />
+      <InputText />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import PrimeButton from "primevue/button";
+import { usePrimeVue } from "primevue/config";
+import { useToast } from "primevue/usetoast";
 
-const cont = ref(0);
+const toast = useToast();
+const $primevue = usePrimeVue();
+
+const show = () => {
+  toast.add({
+    severity: "info",
+    summary: "Info",
+    detail: "Message Content",
+    life: 3000
+  });
+};
+
+defineExpose({
+  $primevue
+});
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
